c31264ac28f08d8f2144a0ae08b87b68f59b1117
Fillipe Meireles <fillipemeireles@Fillipes-Air.intelbras.local>
2024-07-12 14:32:08 -0300

    feat/finish game no assets

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,10 @@ const rl = @cImport({
 const W_WIDTH: c_int = 800;
 const W_HEIGHT: c_int = 450;
 
+const GAME_STAGE = enum {
+    GAMEPLAY,
+    GAME_OVER,
+};
 const GameScreen = struct {
     width: c_int,
     height: c_int,
@@ -18,54 +22,62 @@ const GameScreen = struct {
     }
 };
 
+pub fn GameOverStage() void {
+    rl.ClearBackground(rl.BLACK);
+    rl.DrawText("GAME OVER", W_WIDTH - (W_WIDTH - 30), W_HEIGHT / 2, 120, rl.RED);
+}
+
 const GameObjects = struct {
     rec: rl.Rectangle,
     sprite: rl.Texture2D,
+    color: rl.Color,
 
-    pub fn init(x: f32, y: f32, w: f32, h: f32) GameObjects {
+    pub fn init(x: f32, y: f32, w: f32, h: f32, color: rl.Color) GameObjects {
         return GameObjects{
             .rec = rl.Rectangle{ .x = x, .y = y, .width = w, .height = h },
             .sprite = undefined,
+            .color = color,
         };
     }
     pub fn draw(self: GameObjects) void {
-        rl.DrawRectangleRec(self.rec, rl.WHITE);
+        rl.DrawRectangleRec(self.rec, self.color);
     }
 };
 
 pub const Snake = struct {
     allocator: std.mem.Allocator,
     snode: ?*SNode,
-    step: f32,
 
     pub const SNode = struct {
+        previous_piece: ?*SNode,
         next_piece: ?*SNode,
         piece: GameObjects,
     };
 
-    pub fn init(allocator: std.mem.Allocator) Snake {
-        return .{
-            .allocator = allocator,
-            .snode = null,
-        };
-    }
-
     pub fn initHead(self: *Snake) !void {
         const new_snode = try self.allocator.create(SNode);
         new_snode.* = .{
+            .previous_piece = null,
             .next_piece = null,
-            .piece = GameObjects.init(100, 100, 20, 20),
+            .piece = GameObjects.init(100, 100, 20, 20, rl.GREEN),
         };
 
         self.snode = new_snode;
     }
 
     pub fn sAppend(self: *Snake) !void {
-        if (self.snode) |s| {
+        var l_tail: ?*SNode = null;
+        var p = self.snode;
+        while (p) |s| : (p = s.next_piece) {
+            l_tail = p;
+        }
+
+        if (l_tail) |s| {
             const new_snode = try self.allocator.create(SNode);
             new_snode.* = .{
+                .previous_piece = l_tail,
                 .next_piece = null,
-                .piece = GameObjects.init(s.piece.rec.x + s.piece.rec.width, s.piece.rec.y, 20, 20),
+                .piece = GameObjects.init(s.piece.rec.x + s.piece.rec.width, s.piece.rec.y, 20, 20, rl.WHITE),
             };
 
             s.next_piece = new_snode;
@@ -87,18 +99,75 @@ pub const Snake = struct {
         self.snode = null;
     }
 
-    pub fn moveSnake(self: *Snake, direction: c_int) void {
-        // TODO move head and set tail to previous head pos
+    pub fn moveSnake(self: *Snake, direction: rl.KeyboardKey) void {
+        const head = self.snode orelse return;
+        var step_y: f32 = head.piece.rec.y;
+        var step_x: f32 = head.piece.rec.x;
         if (direction == rl.KEY_DOWN) {
-            var p = self.snode;
-            while (p) |s| : (p = s.next_piece) {
-                s.piece.rec.y += self.step;
-            }
+            step_y += head.piece.rec.height;
         } else if (direction == rl.KEY_LEFT) {
-            var p = self.snode;
-            while (p) |s| : (p = s.next_piece) {
-                s.piece.rec.x -= self.step;
+            step_x -= head.piece.rec.width;
+        } else if (direction == rl.KEY_RIGHT) {
+            step_x += head.piece.rec.width;
+        } else if (direction == rl.KEY_UP) {
+            step_y -= head.piece.rec.height;
+        }
+
+        var np = head.next_piece;
+
+        if (np) |t| {
+            if (t.piece.rec.x == step_x and t.piece.rec.y == step_y) {
+                return;
             }
+        } else {
+            head.piece.rec.x = step_x;
+            head.piece.rec.y = step_y;
+        }
+        var l_tail: ?*SNode = null;
+        while (np) |s| : (np = s.next_piece) {
+            l_tail = np;
+        }
+        while (l_tail) |lt| : (l_tail = lt.previous_piece) {
+            if (lt.previous_piece) |pp| {
+                lt.piece.rec.y = pp.piece.rec.y;
+                lt.piece.rec.x = pp.piece.rec.x;
+            } else {
+                lt.piece.rec.y = step_y;
+                lt.piece.rec.x = step_x;
+            }
+        }
+    }
+
+    pub fn checkSnakeOnWall(self: *Snake) !void {
+        const head = self.snode orelse return;
+        if (head.piece.rec.x <= 0) {
+            head.piece.rec.x = W_WIDTH - head.piece.rec.width;
+        } else if (head.piece.rec.x >= W_WIDTH) {
+            head.piece.rec.x = 0;
+        }
+
+        if (head.piece.rec.y <= 0) {
+            head.piece.rec.y = W_HEIGHT - head.piece.rec.height;
+        } else if (head.piece.rec.y >= W_HEIGHT) {
+            head.piece.rec.y = 0;
+        }
+    }
+
+    pub fn checkSnakeHeadTailCollision(self: *Snake, current_game_stage: *GAME_STAGE) void {
+        const head = self.snode orelse return;
+        var tail = head.next_piece;
+        while (tail) |s| : (tail = s.next_piece) {
+            if (rl.CheckCollisionRecs(head.piece.rec, s.piece.rec)) {
+                current_game_stage.* = GAME_STAGE.GAME_OVER;
+            }
+        }
+    }
+
+    pub fn checkSnakeEated(self: *Snake, apple: *Apple) !void {
+        const head = self.snode orelse return;
+        if (rl.CheckCollisionRecs(head.piece.rec, apple.game_obj.rec)) {
+            try self.sAppend();
+            apple.active = false;
         }
     }
 };
@@ -130,7 +199,7 @@ const Apple = struct {
     }
     pub fn draw(self: Apple) void {
         if (self.active) {
-            rl.DrawRectangleRec(self.game_obj.rec, rl.RED);
+            self.game_obj.draw();
         }
     }
 };
@@ -143,16 +212,18 @@ pub fn main() !void {
     var sll = try allocator.create(Snake);
     defer sll.destroySnake();
     defer allocator.destroy(sll);
-    sll.* = .{ .allocator = allocator, .snode = null, .step = 3 };
+    sll.* = .{ .allocator = allocator, .snode = null };
     try sll.initHead();
     try sll.sAppend();
 
     var apple = Apple{
-        .game_obj = GameObjects.init(0, 0, 10, 10),
+        .game_obj = GameObjects.init(0, 0, 10, 10, rl.RED),
         .active = false,
     };
+
+    var current_game_stage = GAME_STAGE.GAMEPLAY;
     rl.InitWindow(game_screen.width, game_screen.height, "Znake");
-    const interval: f32 = 1;
+    const interval: f32 = 2;
     var timer: f32 = 0;
 
     defer rl.CloseWindow();
@@ -160,19 +231,31 @@ pub fn main() !void {
     while (!rl.WindowShouldClose()) {
         rl.BeginDrawing();
         defer rl.EndDrawing();
-        rl.ClearBackground(rl.BLACK);
         const dt = rl.GetFrameTime();
-        timer += dt;
-        if (timer >= interval) {
-            try apple.update();
-            timer = 0;
-        }
-        const pressed = rl.GetKeyPressed();
-        if (pressed != 0) {
-            sll.moveSnake(pressed);
-        }
+        switch (current_game_stage) {
+            GAME_STAGE.GAMEPLAY => {
+                timer += dt;
+                if (timer >= interval) {
+                    try apple.update();
+                    timer = 0;
+                }
+                var key_pressed: rl.KeyboardKey = 0;
+                if (rl.IsKeyDown(rl.KEY_DOWN)) key_pressed = rl.KEY_DOWN;
+                if (rl.IsKeyDown(rl.KEY_UP)) key_pressed = rl.KEY_UP;
+                if (rl.IsKeyDown(rl.KEY_LEFT)) key_pressed = rl.KEY_LEFT;
+                if (rl.IsKeyDown(rl.KEY_RIGHT)) key_pressed = rl.KEY_RIGHT;
+                if (key_pressed != 0) {
+                    sll.moveSnake(key_pressed);
+                    sll.checkSnakeHeadTailCollision(&current_game_stage);
+                    try sll.checkSnakeOnWall();
+                    try sll.checkSnakeEated(&apple);
+                }
 
-        sll.drawSnake();
-        apple.draw();
+                sll.drawSnake();
+                apple.draw();
+            },
+            GAME_STAGE.GAME_OVER => GameOverStage(),
+        }
+        rl.ClearBackground(rl.BLACK);
     }
 }
